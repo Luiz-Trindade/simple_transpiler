f3cef0fa5dd7261d5f155d178fb129931ce092fb
Luiz Trindade <149267777+Luiz-Trindade@users.noreply.github.com>
2023-12-16 20:45:18 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ This is a simple transpiler written in Python by Luiz Gabriel Magalhães Trindad
 - **Print Statements:** Use the `print` statement to display content on the console.
 - **Variable Declaration:** Declare variables with the `number` and `string` statements.
 - **Variable View:** View the value of a variable using the `view` statement.
+- **Keyboard Input Data** Use the `input` command to recieve keyboard data.
 - **Math Operations:** Perform basic mathematical operations with the `math` statement.
 - **System Commands:** Execute system commands using the `SYSTEM` statement.
 - **Control Flow:** Use labels (`section` and `goto`) to control the flow of the program.
@@ -34,14 +35,14 @@ number result 0
 
 string greeting Hello from the transpiler
 
-print \n
+newline
 view x
-print \n
+newline
 view greeting
-print \n
+newline
 
 math n1 + n2 result
 
 print The result is:
 view result
-print \n
+newline
--- simple_trasnpiler.py
+++ simple_trasnpiler.py
@@ -7,19 +7,16 @@ from sys import argv
 from os import system as S
 
 c_compiler = "tcc"
-
 program = str(argv[1])
-
 source = []
-
 c_source = [
     "#include <stdio.h>", 
     "#include <stdlib.h>", 
     "#include <string.h>", 
     "int main(){"
     ]
-    
 var = {}
+buffer = int(500)
 
 # Debug Function.
 def View_C_Code():
@@ -38,7 +35,7 @@ def Read_Source():
         source = [f.strip() for f in file]
 
 def Transpile_To_C():
-    global source, c_compiler, c_source, var
+    global source, c_compiler, c_source, var, buffer
     counter = 0
     while counter < len(source):
         command = source[counter].split(" ")
@@ -57,14 +54,28 @@ def Transpile_To_C():
         elif command[0] == "string":
             name = str(command[1])
             content = str(" ".join(command[2:]))
-            length = len(content)+1
-            var[name] = str("%s")
-            c_source.append(f'char {name}[{length}] = "{content}";')
+            if content == "":
+                length = 500
+                var[name] = str("%s")
+                c_source.append(f'char {name}[{length}];')
+            else:    
+                length = len(content)+1
+                var[name] = str("%s")
+                c_source.append(f'char {name}[{length}] = "{content}";')
 
         elif command[0] == "view":
             name = str(command[1])
             var_type = var[name]
             c_source.append(f'printf("{var_type}", {name});')
+
+        elif command[0] == "input":
+            var_name = command[1]
+            if var[var_name] == "%f":
+                c_source.append(f'scanf("%f", &{var_name});')
+            else:
+                c_source.append("setbuf(stdin, NULL);")
+                c_source.append(f"fgets({var_name}, {buffer}, stdin);") 
+
 
         #Labels
         elif command[0] == "section":
@@ -86,6 +97,21 @@ def Transpile_To_C():
             cmd = " ".join(command[1:])
             c_source.append(f'system("{cmd}");')
 
+        elif command[0] == "comp":
+            var1 = command[1]
+            oper = command[2]
+            var2 = command[3]
+            section_to_go = command[4]
+            if var[var1] == "%f" and var[var2] == "%f":
+                c_source.append(f'if ({var1} {oper} {var2})')
+                c_source.append("{goto "+section_to_go+";}")
+            else:
+                c_source.append(f'if (strcmp({var1}, {var2}) == 0)')
+                c_source.append("{goto "+section_to_go+";}")
+
+        elif command[0] == "newline":
+            c_source.append('printf("\\n");')
+
         #Forçar o fim da execução do programa
         elif command[0] == "end":
             c_source.append("break;")
@@ -95,13 +121,15 @@ def Transpile_To_C():
     c_source.append("}")
 
 def Make_Executable():
-    global programa, c_source, c_compiler
+    global program, c_source, c_compiler
     with open(f"{program}.c", "w") as c_file:
         c_file.writelines("\n".join(c_source))
-    try: 
+    try:
+        print(f"Using '{c_compiler}' as C compiler!")
         S(f"{c_compiler} -Ofast -flto -finline-functions -funroll-loops -ftree-vectorize {program}.c -o {program}_exec")
         S(f"rm {program}.c")
-    except: 
+    except:
+        print("Using 'gcc' as C compiler")
         S(f"gcc -Ofast -flto -finline-functions -funroll-loops -ftree-vectorize {program}.c -o {program}_exec")
         S(f"rm {program}.c")
 
